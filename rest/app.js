50b9966a2fd58a89ff70acc08639b1c150d053e0
RSWeslei <weslei67rosiak.com@gmail.com>
2022-05-11 17:39:49 -0300

    w

--- rest/app.js
+++ rest/app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 
 const routeProducts = require('./routes/product') 
 
-app.use(morgan('dev')) //para fazer o log das requisições
+app.use(morgan('dev')) //para fazer o log das requisicoes
 app.use(bodyParser.urlencoded ({ extended: false })) //para fazer apenas requisições com dados simples  
 app.use(bodyParser.json()) //aceita apenas json
 
